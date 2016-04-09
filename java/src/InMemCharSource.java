e56e769e9160d87b8f0384f2c43adbe5100f5820
Matt Powell <mttpwll@gmail.com>
2016-04-09 13:11:16 -0700

    quick fix to support Java <8

--- java/src/InMemCharSource.java
+++ java/src/InMemCharSource.java
@@ -4,7 +4,7 @@ import java.io.StringReader;
 import java.io.Reader;
 
 public class InMemCharSource {
-  public static CharSource create(String contentString) {
+  public static CharSource create(final String contentString) {
     CharSource source = new CharSource() {
       @Override
       public Reader openStream() {
